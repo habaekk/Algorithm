f0ec287aa94c1941e3a49e03c3b85ff2c8efca93
habaek <74465964+habaekk@users.noreply.github.com>
2023-06-08 23:00:05 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 1700 ms, Memory: 258416 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Main {
+    static int V, E;
+    static int[][] graph;
+    static int[] parent;
+    static int final_cost;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        V = sc.nextInt();
+        E = sc.nextInt();
+
+        graph = new int[E][3];
+        for (int i = 0; i < E; i++) {
+            graph[i][0] = sc.nextInt();
+            graph[i][1] = sc.nextInt();
+            graph[i][2] = sc.nextInt();
+        }
+        parent = new int[V];
+        final_cost = 0;
+
+        Arrays.sort(graph, ((o1, o2) -> Integer.compare(o1[2], o2[2])));
+
+        for (int i = 0; i < V; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < E; i++) {
+            if(find(graph[i][0] - 1) != (find(graph[i][1] - 1))) {
+                union(graph[i][0] - 1, graph[i][1] - 1);
+                final_cost += graph[i][2];
+                continue;
+            }
+        }
+
+        System.out.println(final_cost);
+    }
+
+
+
+
+    private static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a > b) {
+            parent[a] = b;
+        } else {
+            parent[b] = a;
+        }
+    }
+    private static int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        } else {
+            return find(parent[x]);
+        }
+    }
+}

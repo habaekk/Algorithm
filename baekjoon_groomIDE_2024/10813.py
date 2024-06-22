997c5554a24a8c3bcf18b13d6996fa2c254ebdca
Habaek Bak <haback1493@gmail.com>
2024-06-22 03:09:02 +0000

    initial commit for groomIDE

---
+++ baekjoon_groomIDE_2024/10813.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.read
+
+data = list(map(int, input().split()))
+
+n = data[0]
+m = data[1]
+
+lst = []
+for i in range(1, n+1):
+    lst.append(i)
+
+idx1 = 2
+for _ in range(m):
+    i = data[idx1]
+    j = data[idx1 + 1]
+    tmp = lst[i-1]
+    lst[i-1] = lst[j-1]
+    lst[j-1] = tmp
+    idx1 += 2
+
+print(" ".join(map(str, lst)))

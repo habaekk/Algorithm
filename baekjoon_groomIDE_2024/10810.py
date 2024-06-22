b1282445d4453bd6f68d2a01b0827532dead9bf7
Habaek Bak <haback1493@gmail.com>
2024-06-22 03:20:47 +0000

    10810 revised

--- baekjoon_groomIDE_2024/10810.py
+++ baekjoon_groomIDE_2024/10810.py
@@ -1,19 +1,43 @@
+### ChatGPT revised ###
+
 import sys
 input = sys.stdin.read
-data = input().split()
+data = list(map(int, input().split()))
 
-n = int(data[0])
-m = int(data[1])
+n = data[0]
+m = data[1]
 
-lst = [0] * n
+lst = list(range(1, n+1))
 
-index = 2
+idx1 = 2
 for _ in range(m):
-    i = int(data[index])
-    j = int(data[index + 1])
-    k = int(data[index + 2])
-    for idx in range(i - 1, j):
-        lst[idx] = k
-    index += 3
-
+    i = data[idx1]
+    j = data[idx1+1]
+    lst[i-1], lst[j-1] = lst[j-1], lst[i-1]
+    idx1 += 2
+    
 print(" ".join(map(str, lst)))
+
+
+
+### ORIGINAL ###
+
+# import sys
+# input = sys.stdin.read
+# data = input().split()
+
+# n = int(data[0])
+# m = int(data[1])
+
+# lst = [0] * n
+
+# index = 2
+# for _ in range(m):
+#     i = int(data[index])
+#     j = int(data[index + 1])
+#     k = int(data[index + 2])
+#     for idx in range(i - 1, j):
+#         lst[idx] = k
+#     index += 3
+
+# print(" ".join(map(str, lst)))
